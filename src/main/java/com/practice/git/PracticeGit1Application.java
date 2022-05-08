177833ba1f30e2909d95eacd6e9b42f2f9de6b71
Rajesh Sirohi <rsirohikoviya@yahoo.com>
2022-05-08 12:39:31 -0700

    first commit

---
+++ src/main/java/com/practice/git/PracticeGit1Application.java
@@ -0,0 +1,13 @@
+package com.practice.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGit1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/git/PracticeGit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
