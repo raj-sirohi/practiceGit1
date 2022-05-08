bc4c445a79a7439c5bf7315873ef1f69bc619e83
raj-sirohi <rsirohikoviya@yahoo.com>
2022-05-08 13:38:49 -0700

    second commit - in git

--- src/main/java/com/practice/git/PracticeGit1Application.java
+++ src/main/java/com/practice/git/PracticeGit1Application.java
@@ -2,7 +2,7 @@ package com.practice.git;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// second commit in git
 @SpringBootApplication
 public class PracticeGit1Application {
 
